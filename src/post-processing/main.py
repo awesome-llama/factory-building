29087f7030d4914d3b7a8b3dd2d38d7c0115ada9
awesome-llama <17157128+awesome-llama@users.noreply.github.com>
2026-05-20 02:37:07 +1000

    remove costume post processing

--- src/post-processing/main.py
+++ src/post-processing/main.py
@@ -15,26 +15,5 @@ with post_processing.ScratchProject(PATH_SOURCE_PROJECT, PATH_TARGET_PROJECT) as
     project.clean_up_blocks()
     project.remove_field_text()
     project.move_turbowarp_comment()
-
-    
-    # Fix the costume sizes
-
-    # backdrop checker
-    costume = project.get_costume_by_name('Stage', 'darkchecker')
-    costume['bitmapResolution'] = 1
-    # for some reason, not setting these lets the backdrop work for Scratch *and* TurboWarp custom resolution:
-    if 'rotationCenterX' in costume: costume.pop('rotationCenterX')
-    if 'rotationCenterY' in costume: costume.pop('rotationCenterY')
-
-    # thumbnail (960x720)
-    costume = project.get_costume_by_name('_', 'awesome-llama')
-    costume['bitmapResolution'] = 2
-    costume['rotationCenterX'] = 480
-    costume['rotationCenterY'] = 360
-
-
-
-
     project.add_build_comment('Factory Building\nCreated by awesome-llama\nhttps://scratch.mit.edu/projects/773175240\nhttps://github.com/awesome-llama/factory-building')
-
 
